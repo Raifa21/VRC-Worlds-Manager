979b9c7c14fce59fac3b46b885dfc9d741814950
Raifa21 <79643312+Raifa21@users.noreply.github.com>
2024-07-15 16:25:33 +0700

    Created models to fit API usage

---
+++ VRC Favourite Manager/Models/WorldModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRC_Favourite_Manager
+{
+    public class WorldModel
+    {
+        public string imageUrl { get; set; }
+        public string name { get; set; }
+        public string recommendedCapacity { get; set; }
+        public string capacity { get; set; }
+    }
+
+}
